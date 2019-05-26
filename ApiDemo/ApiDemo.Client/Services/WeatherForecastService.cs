eaa85d5bcadf09441394047f4d2035d168467d9c
Daniel Plawgo <daniel@plawgo.pl>
2019-05-26 07:06:34 +0200

    dodanie przykładu

---
+++ ApiDemo/ApiDemo.Client/Services/WeatherForecastService.cs
@@ -0,0 +1,30 @@
+﻿using ApiDemo.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ApiDemo.Client.Services
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly HttpClient _client;
+
+        public WeatherForecastService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public async Task<WeatherForecast[]> GetAsync()
+        {
+            return await _client.GetJsonAsync<WeatherForecast[]>("api/SampleData/WeatherForecasts");
+        }
+    }
+
+    public interface IWeatherForecastService
+    {
+        Task<WeatherForecast[]> GetAsync();
+    }
+}
---
+++ ApiDemo/ApiDemo.Client/Startup.cs
@@ -0,0 +1,19 @@
+using ApiDemo.Client.Services;
+using Microsoft.AspNetCore.Components.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ApiDemo.Client
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+        }
+
+        public void Configure(IComponentsApplicationBuilder app)
+        {
+            app.AddComponent<App>("app");
+        }
+    }
+}
